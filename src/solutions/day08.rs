007e16e4af13f25ced1515e43f51a489d3975ba0
Stephen Weinberg <stephenmweinberg@gmail.com>
2023-12-08 10:49:08 -0600

    Day 8: remove hashmap from critical path
    Saves 5 to 6ms..

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -3,22 +3,28 @@ use ahash::AHashMap;
 use crate::{solutions::prelude::*, utils::lcm};
 
 pub fn problem1(input: &str) -> Result<String, anyhow::Error> {
-    let (dir, nodes) = parse!(input);
-    let node_map = AHashMap::from_iter(nodes.iter().map(|x| (x.id.as_str(), x)));
+    let (dir, node_descs) = parse!(input);
+    let nodes = build_nodes(node_descs)?;
 
-    let start = node_map.get("AAA").unwrap();
-    let ans = moves_until_end(start, &node_map, &dir, |x| x == "ZZZ");
+    let start = nodes
+        .iter()
+        .position(|x| x.id == "AAA")
+        .ok_or_else(|| anyhow!("no starting node found"))?;
+    let ans = moves_until_end(start, &nodes, &dir, |x| x == "ZZZ");
 
     Ok(ans.to_string())
 }
 
 pub fn problem2(input: &str) -> Result<String, anyhow::Error> {
-    let (dir, nodes) = parse!(input);
-    let node_map = AHashMap::from_iter(nodes.iter().map(|x| (x.id.as_str(), x)));
+    let (dir, node_descs) = parse!(input);
+    let nodes = build_nodes(node_descs)?;
 
-    let starts = nodes.iter().filter(|x| x.id.ends_with("A"));
+    let starts = nodes
+        .iter()
+        .enumerate()
+        .filter(|(_, x)| x.id.ends_with("A"));
     let cycle_lengths =
-        starts.map(|s| moves_until_end(s, &node_map, &dir, |id| id.ends_with("Z")) as u64);
+        starts.map(|(s, _)| moves_until_end(s, &nodes, &dir, |id| id.ends_with("Z")) as u64);
     let cycles_lcm = cycle_lengths
         .reduce(lcm)
         .ok_or_else(|| anyhow!("no starting nodes found"))?;
@@ -26,19 +32,50 @@ pub fn problem2(input: &str) -> Result<String, anyhow::Error> {
     Ok(cycles_lcm.to_string())
 }
 
+fn build_nodes(mut descs: Vec<NodeDesc>) -> Result<Vec<Node>, anyhow::Error> {
+    let mut nodes: Vec<Node> = descs
+        .iter_mut()
+        .map(|x| Node {
+            id: std::mem::take(&mut x.id),
+            left: 0,
+            right: 0,
+        })
+        .collect();
+
+    let m: AHashMap<String, usize> = nodes
+        .iter()
+        .enumerate()
+        .map(|(i, n)| (n.id.clone(), i))
+        .collect();
+
+    for (i, node) in nodes.iter_mut().enumerate() {
+        let left_id = descs[i].left.as_str();
+        let right_id = descs[i].right.as_str();
+
+        node.left = *m
+            .get(left_id)
+            .ok_or_else(|| anyhow!("node `{}` not declared", left_id))?;
+        node.right = *m
+            .get(right_id)
+            .ok_or_else(|| anyhow!("node `{}` not declared", right_id))?;
+    }
+
+    Ok(nodes)
+}
+
 fn moves_until_end(
-    start: &Node,
-    nodes: &AHashMap<&str, &Node>,
+    start: usize,
+    nodes: &[Node],
     directions: &[Direction],
     predicate: impl Fn(&str) -> bool,
 ) -> usize {
     let mut cur = start;
     let mut count = 0;
     for &d in directions.iter().cycle() {
-        if predicate(&cur.id) {
+        if predicate(&nodes[cur].id) {
             break;
         }
-        cur = nodes.get(cur.get(d)).unwrap();
+        cur = nodes[cur].get(d);
         count += 1;
     }
 
@@ -52,17 +89,24 @@ enum Direction {
 }
 
 #[derive(Clone, Debug)]
-struct Node {
+struct NodeDesc {
     id: String,
     left: String,
     right: String,
 }
 
+#[derive(Clone, Debug)]
+struct Node {
+    id: String,
+    left: usize,
+    right: usize,
+}
+
 impl Node {
-    fn get(&self, d: Direction) -> &str {
+    fn get(&self, d: Direction) -> usize {
         match d {
-            Direction::Left => &self.left,
-            Direction::Right => &self.right,
+            Direction::Left => self.left,
+            Direction::Right => self.right,
         }
     }
 }
@@ -73,7 +117,7 @@ mod parser {
     use super::*;
     use crate::parser::prelude::*;
 
-    pub fn parse(input: &str) -> IResult<&str, (Vec<Direction>, Vec<Node>)> {
+    pub fn parse(input: &str) -> IResult<&str, (Vec<Direction>, Vec<NodeDesc>)> {
         let dir = alt((
             value(Direction::Left, char('L')),
             value(Direction::Right, char('R')),
@@ -86,7 +130,7 @@ mod parser {
             char(')'),
         );
         let node = separated_pair(node_id(), tag(" = "), node_value)
-            .map(|(id, (left, right))| Node { id, left, right });
+            .map(|(id, (left, right))| NodeDesc { id, left, right });
 
         let nodes = many1(ws_line(node));
         let dir_line = ws_line(dir_list);
