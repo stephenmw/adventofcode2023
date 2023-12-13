b5ac8be1e24b40c512cd0cc6409f6b8f042abb49
Stephen Weinberg <stephenmweinberg@gmail.com>
2023-12-13 00:03:47 -0600

    Day 13 Part 2

--- src/solutions/day13.rs
+++ src/solutions/day13.rs
@@ -5,56 +5,73 @@ pub fn problem1(input: &str) -> Result<String, anyhow::Error> {
     let grids = parse!(input);
     let ans: usize = grids
         .iter()
-        .map(|g| find_v_mirror(g).unwrap_or(0) + find_h_mirror(g).unwrap_or(0) * 100)
+        .map(|g| find_v_mirror(g, 0).unwrap_or(0) + find_h_mirror(g, 0).unwrap_or(0) * 100)
         .sum();
     Ok(ans.to_string())
 }
 
-pub fn problem2(_input: &str) -> Result<String, anyhow::Error> {
-    todo!()
+pub fn problem2(input: &str) -> Result<String, anyhow::Error> {
+    let grids = parse!(input);
+    let ans: usize = grids
+        .iter()
+        .map(|g| find_v_mirror(g, 1).unwrap_or(0) + find_h_mirror(g, 1).unwrap_or(0) * 100)
+        .sum();
+    Ok(ans.to_string())
 }
 
-fn find_v_mirror(grid: &Grid<Cell>) -> Option<usize> {
+fn find_v_mirror(grid: &Grid<Cell>, num_smudges: usize) -> Option<usize> {
     let (cols, rows) = grid.size();
 
     'mirror: for mirror in 1..cols {
-        for c in mirror..cols {
+        let mut num_diffs = 0;
+        'cols: for c in mirror..cols {
             for r in 0..rows {
                 let reflection = (2 * mirror).checked_sub(c + 1).map(|c| Point::new(c, r));
                 let Some(reflect_val) = reflection.and_then(|p| grid.get(p)) else {
-                    return Some(mirror);
+                    break 'cols;
                 };
                 let val = grid.get(Point::new(c, r)).unwrap();
                 if reflect_val != val {
-                    continue 'mirror;
+                    num_diffs += 1;
+                    if num_diffs > num_smudges {
+                        continue 'mirror;
+                    }
                 }
             }
         }
 
-        return Some(mirror);
+        if num_diffs == num_smudges {
+            return Some(mirror);
+        }
     }
 
     None
 }
 
-fn find_h_mirror(grid: &Grid<Cell>) -> Option<usize> {
+fn find_h_mirror(grid: &Grid<Cell>, num_smudges: usize) -> Option<usize> {
     let (cols, rows) = grid.size();
 
     'mirror: for mirror in 1..rows {
-        for r in mirror..rows {
+        let mut num_diffs = 0;
+        'rows: for r in mirror..rows {
             for c in 0..cols {
                 let reflection = (2 * mirror).checked_sub(r + 1).map(|r| Point::new(c, r));
                 let Some(reflect_val) = reflection.and_then(|p| grid.get(p)) else {
-                    return Some(mirror);
+                    break 'rows;
                 };
                 let val = grid.get(Point::new(c, r)).unwrap();
                 if reflect_val != val {
-                    continue 'mirror;
+                    num_diffs += 1;
+                    if num_diffs > num_smudges {
+                        continue 'mirror;
+                    }
                 }
             }
         }
 
-        return Some(mirror);
+        if num_diffs == num_smudges {
+            return Some(mirror);
+        }
     }
 
     None
@@ -110,6 +127,6 @@ mod tests {
 
     #[test]
     fn problem2_test() {
-        //assert_eq!(problem2(EXAMPLE_INPUT).unwrap(), "400")
+        assert_eq!(problem2(EXAMPLE_INPUT).unwrap(), "400")
     }
 }
