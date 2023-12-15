b5537c6fd79ec295f21dc942c3f6c805c2ab7a7b
Stephen Weinberg <stephenmweinberg@gmail.com>
2023-12-14 22:23:31 -0600

    Day 13: rewrite using compressed grid.

--- src/solutions/day13.rs
+++ src/solutions/day13.rs
@@ -5,7 +5,10 @@ pub fn problem1(input: &str) -> Result<String, anyhow::Error> {
     let grids = parse!(input);
     let ans: usize = grids
         .iter()
-        .map(|g| find_v_mirror(g, 0).unwrap_or(0) + find_h_mirror(g, 0).unwrap_or(0) * 100)
+        .map(|g| {
+            let cg = CompressedGrid::try_from(g).unwrap();
+            find_mirror(&cg.rows, 0) * 100 + find_mirror(&cg.columns, 0)
+        })
         .sum();
     Ok(ans.to_string())
 }
@@ -14,55 +17,59 @@ pub fn problem2(input: &str) -> Result<String, anyhow::Error> {
     let grids = parse!(input);
     let ans: usize = grids
         .iter()
-        .map(|g| find_v_mirror(g, 1).unwrap_or(0) + find_h_mirror(g, 1).unwrap_or(0) * 100)
+        .map(|g| {
+            let cg = CompressedGrid::try_from(g).unwrap();
+            find_mirror(&cg.rows, 1) * 100 + find_mirror(&cg.columns, 1)
+        })
         .sum();
     Ok(ans.to_string())
 }
 
-fn find_v_mirror(grid: &Grid<Cell>, num_smudges: usize) -> Option<usize> {
-    fn reflect(p: Point, mirror: usize) -> Point {
-        let col = 2 * mirror - p.x - 1;
-        Point::new(col, p.y)
-    }
-
-    let (cols, rows) = grid.size();
-
-    (1..cols).find(|&mirror| {
-        let left_points = (0..mirror)
-            .rev()
-            .flat_map(|c| (0..rows).map(move |r| Point::new(c, r)));
-        let diffs = left_points
-            .map_while(|p| Some(grid.get(p).unwrap() == grid.get(reflect(p, mirror))?))
-            .filter(|&x| x == false)
-            .count();
-        diffs == num_smudges
-    })
+#[derive(Clone, Debug)]
+struct CompressedGrid {
+    rows: Vec<u32>,
+    columns: Vec<u32>,
 }
 
-fn find_h_mirror(grid: &Grid<Cell>, num_smudges: usize) -> Option<usize> {
-    fn reflect(p: Point, mirror: usize) -> Point {
-        let row = 2 * mirror - p.y - 1;
-        Point::new(p.x, row)
+impl TryFrom<&Grid<bool>> for CompressedGrid {
+    type Error = anyhow::Error;
+
+    fn try_from(grid: &Grid<bool>) -> Result<Self, Self::Error> {
+        let (col_len, row_len) = grid.size();
+        if col_len > 32 || row_len > 32 {
+            bail!("grid too big to be compressed");
+        }
+
+        let rows = grid
+            .cells
+            .iter()
+            .map(|r| r.iter().fold(0, |acc, &x| (acc << 1) + x as u32))
+            .collect();
+
+        let columns = (0..col_len)
+            .map(|col| {
+                (0..row_len)
+                    .map_while(|row| grid.get(Point::new(col, row)))
+                    .fold(0, |acc, &x| (acc << 1) + x as u32)
+            })
+            .collect();
+
+        Ok(CompressedGrid { rows, columns })
     }
-
-    let (cols, rows) = grid.size();
-
-    (1..rows).find(|&mirror| {
-        let top_points = (0..mirror)
-            .rev()
-            .flat_map(|r| (0..cols).map(move |c| Point::new(c, r)));
-        let diffs = top_points
-            .map_while(|p| Some(grid.get(p).unwrap() == grid.get(reflect(p, mirror))?))
-            .filter(|&x| x == false)
-            .count();
-        diffs == num_smudges
-    })
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum Cell {
-    Ash,
-    Rock,
+fn find_mirror(lines: &[u32], target_diff: u32) -> usize {
+    (1..lines.len())
+        .find(|&mirror| {
+            let diffs: u32 = lines[..mirror]
+                .iter()
+                .rev()
+                .zip(&lines[mirror..])
+                .map(|(&a, &b)| (a ^ b).count_ones())
+                .sum();
+            diffs == target_diff
+        })
+        .unwrap_or(0)
 }
 
 mod parser {
@@ -71,8 +78,8 @@ mod parser {
     use super::*;
     use crate::parser::prelude::*;
 
-    pub fn parse(input: &str) -> IResult<&str, Vec<Grid<Cell>>> {
-        let cell = alt((value(Cell::Ash, char('.')), value(Cell::Rock, char('#'))));
+    pub fn parse(input: &str) -> IResult<&str, Vec<Grid<bool>>> {
+        let cell = alt((value(false, char('.')), value(true, char('#'))));
         let row = ws_line(many1(cell));
         let grid = many1(row).map(|rows| Grid::new(rows));
         let parser = separated_list1(multispace1, grid);
