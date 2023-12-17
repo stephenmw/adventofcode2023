0bb966045c840aeb91240f32ded48d3e38daa981
Stephen Weinberg <stephenmweinberg@gmail.com>
2023-12-17 00:12:40 -0600

    Day 17 Part 1

---
+++ src/solutions/day17.rs
@@ -0,0 +1,124 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+use ahash::HashSet;
+
+use crate::grid::{Direction, Grid, Point};
+use crate::solutions::prelude::*;
+use crate::utils::HeapElement;
+
+pub fn problem1(input: &str) -> Result<String, anyhow::Error> {
+    let grid = parse!(input);
+    let (cols, rows) = grid.size();
+    let target_point = Point::new(cols - 1, rows - 1);
+
+    let mut seen = HashSet::default();
+    let mut frontier = BinaryHeap::new();
+
+    let start = WalkState {
+        loc: Point::new(0, 0),
+        dir: Direction::Right,
+        straight_count: 0,
+    };
+
+    for state in start.iter_next() {
+        frontier.push(Reverse(HeapElement::from((0usize, state))));
+    }
+
+    while let Some(elem) = frontier.pop().map(|x| x.0) {
+        let Some(&additional_cost) = grid.get(elem.value.loc) else {
+            continue;
+        };
+
+        let new_cost = elem.key + additional_cost as usize;
+
+        if elem.value.loc == target_point {
+            return Ok(new_cost.to_string());
+        }
+
+        for next_state in elem.value.iter_next() {
+            if seen.insert(next_state.clone()) {
+                frontier.push(Reverse((new_cost, next_state).into()));
+            }
+        }
+    }
+
+    unreachable!("no solution");
+}
+
+pub fn problem2(_input: &str) -> Result<String, anyhow::Error> {
+    todo!()
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct WalkState {
+    loc: Point,
+    dir: Direction,
+    straight_count: u8,
+}
+
+impl WalkState {
+    fn iter_next(&self) -> impl Iterator<Item = Self> + '_ {
+        let dirs = [
+            Some(self.dir.rotate_left()),
+            (self.straight_count < 3).then_some(self.dir),
+            Some(self.dir.rotate_right()),
+        ];
+
+        dirs.into_iter().filter_map(|d| {
+            let d = d?;
+            Some(WalkState {
+                loc: self.loc.next(d)?,
+                dir: d,
+                straight_count: if d == self.dir {
+                    self.straight_count + 1
+                } else {
+                    1
+                },
+            })
+        })
+    }
+}
+
+mod parser {
+    use super::*;
+    use crate::parser::prelude::*;
+
+    pub fn parse(input: &str) -> IResult<&str, Grid<u8>> {
+        let cell = one_of("0123456789").map(|x| x.to_digit(10).unwrap() as u8);
+        let row = ws_line(many1(cell));
+        let grid = many1(row).map(|d| Grid::new(d));
+        ws_all_consuming(grid)(input)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = "
+        2413432311323
+        3215453535623
+        3255245654254
+        3446585845452
+        4546657867536
+        1438598798454
+        4457876987766
+        3637877979653
+        4654967986887
+        4564679986453
+        1224686865563
+        2546548887735
+        4322674655533
+    ";
+
+    #[test]
+    fn problem1_test() {
+        assert_eq!(problem1(EXAMPLE_INPUT).unwrap(), "102")
+    }
+
+    #[test]
+    fn problem2_test() {
+        assert_eq!(problem2(EXAMPLE_INPUT).unwrap(), "71")
+    }
+}
